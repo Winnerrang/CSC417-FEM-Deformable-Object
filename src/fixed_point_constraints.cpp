4a0e8b19d9713ab86a884dc68fe805e8eb0faba5
Winnerrang <leo.hanxu@mail.utoronto.ca>
2024-01-04 15:47:28 -0500

    copy fixed_point_constraint code from lab 2

--- src/fixed_point_constraints.cpp
+++ src/fixed_point_constraints.cpp
@@ -2,6 +2,27 @@
 #include <algorithm>
 void fixed_point_constraints(Eigen::SparseMatrixd &P, unsigned int q_size, const std::vector<unsigned int> indices) {
 
-    
+	for (int i = 0; i < indices.size(); i++) {
+		unsigned int vertexIndex = indices[i];
+
+		for (int dimension = 0; dimension < 3; dimension++) {
+			P.coeffRef(3 * vertexIndex + dimension, 3 * vertexIndex + dimension) = 0;
+		}
+	}
+
+	// remove rows with all zeros
+	Eigen::SparseMatrixd newP(q_size - 3 * indices.size(), q_size);
+
+	int newRow = 0;
+	for (int row = 0; row < P.rows(); row++) {
+		int col = row;
+		if (P.coeff(row, col) == 0) continue;
+
+		newP.coeffRef(newRow, col) = P.coeff(row, col);
+
+		newRow++;
+	}
+
+	P = newP;
 
 }
