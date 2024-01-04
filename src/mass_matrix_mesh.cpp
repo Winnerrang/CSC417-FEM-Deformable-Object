bd5e7370711ef36f690d05ffe3d5c1be2df8d6e5
Winnerrang <leo.hanxu@mail.utoronto.ca>
2024-01-04 15:34:30 -0500

    Fix bug in mass matrix construction

--- src/mass_matrix_mesh.cpp
+++ src/mass_matrix_mesh.cpp
@@ -14,17 +14,18 @@ void mass_matrix_mesh(Eigen::SparseMatrixd &M, Eigen::Ref<const Eigen::VectorXd>
         // find qdot for this tetrahedron
         qdot_tetra << qdot.segment<3>(T(tetraIdx, 0) * 3), qdot.segment<3>(T(tetraIdx, 1) * 3), qdot.segment<3>(T(tetraIdx, 2) * 3), qdot.segment<3>(T(tetraIdx, 3) * 3);
 
-
         mass_matrix_linear_tetrahedron(M_tetra, qdot_tetra, tetra_indices, density, v0(tetraIdx));
 
         for (int row = 0; row < M_tetra.rows(); row++) {
             for (int col = 0; col < M_tetra.cols(); col++) {
 
-                int targetRow = tetra_indices(row / 4) * 3 + row % 3;
-                int targetCol = tetra_indices(col / 4) * 3 + col % 3;
+                int targetRow = tetra_indices(row / 3) * 3 + row % 3;
+                int targetCol = tetra_indices(col / 3) * 3 + col % 3;
 				M.coeffRef(targetRow, targetCol) += M_tetra(row, col);
 			}
 		}
     }
+
+    
 
 }
