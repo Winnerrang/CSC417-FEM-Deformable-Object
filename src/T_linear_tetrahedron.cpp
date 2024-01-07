76a1f010a3ff8a5bb7817e235c6a9f6d93014315
Winnerrang <leo.hanxu@mail.utoronto.ca>
2024-01-07 18:08:24 -0500

    Finish calculating kinetic energy

--- src/T_linear_tetrahedron.cpp
+++ src/T_linear_tetrahedron.cpp
@@ -4,5 +4,7 @@
 void T_linear_tetrahedron(double &T, Eigen::Ref<const Eigen::VectorXd> qdot, Eigen::Ref<const Eigen::RowVectorXi> element, double density, double volume) {
 
     
-        
+    Eigen::Matrix1212d M;
+	mass_matrix_linear_tetrahedron(M, qdot, element, density, volume);
+	T = 0.5 * qdot.transpose() * M * qdot;
 }
