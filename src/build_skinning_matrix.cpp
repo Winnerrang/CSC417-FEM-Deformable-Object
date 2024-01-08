cf6c7ab794e3e92bc0b4d817462c0d373706d6e6
Winnerrang <leo.hanxu@mail.utoronto.ca>
2024-01-08 18:18:12 -0500

    finsih skinning matrix

--- src/build_skinning_matrix.cpp
+++ src/build_skinning_matrix.cpp
@@ -2,9 +2,52 @@
 #include <phi_linear_tetrahedron.h>
 #include <vector>
 #include <iostream>
+#include <cassert>
+bool sameSide(Eigen::Ref<const Eigen::RowVector3d> p1,
+    Eigen::Ref<const Eigen::RowVector3d> p2,
+    Eigen::Ref<const Eigen::RowVector3d> p3,
+    Eigen::Ref<const Eigen::RowVector3d> p4,
+    Eigen::Ref<const Eigen::RowVector3d> c) {
+    Eigen::RowVector3d normal = (p2 - p1).cross(p3 - p1);
 
+    return (p4 - p1).dot(normal) * (c - p1).dot(normal) >= 0;
+
+}
 void build_skinning_matrix(Eigen::SparseMatrixd &N, Eigen::Ref<const Eigen::MatrixXd> V, Eigen::Ref<const Eigen::MatrixXi> T, 
                                                    Eigen::Ref<const Eigen::MatrixXd> V_skin) {
 
-    
+    // assume the order of vertex in V is the order of the generalized coordinate q
+    N.resize(V_skin.rows() * 3, V.rows() * 3);
+    N.setZero();
+
+
+    for (int vertexIdx = 0; vertexIdx < V_skin.rows(); vertexIdx++) {
+        Eigen::Vector4d phi;
+
+        int tetra_idx = -1;
+        for (int tetIdx = 0; tetIdx < T.rows(); tetIdx++) {
+			Eigen::RowVector4i tet = T.row(tetIdx);
+            Eigen::RowVector3d p1 = V.row(tet(0));
+			Eigen::RowVector3d p2 = V.row(tet(1));
+            Eigen::RowVector3d p3 = V.row(tet(2));
+			Eigen::RowVector3d p4 = V.row(tet(3));
+			Eigen::RowVector3d c = V_skin.row(vertexIdx);
+
+			if (sameSide(p1, p2, p3, p4, c) && sameSide(p2, p3, p4, p1, c) && sameSide(p3, p4, p1, p2, c) && sameSide(p4, p1, p2, p3, c)) {
+				tetra_idx = tetIdx;
+                break;
+            }
+
+		}
+
+        assert(tetra_idx != -1);
+        phi_linear_tetrahedron(phi, V, T.row(tetra_idx), V_skin.row(vertexIdx).transpose());
+        
+        for (int dim = 0; dim < 3; dim++) {
+			N.coeffRef(3 * vertexIdx + dim, 3 * T(tetra_idx, 0) + dim) = phi(0);
+			N.coeffRef(3 * vertexIdx + dim, 3 * T(tetra_idx, 1) + dim) = phi(1);
+			N.coeffRef(3 * vertexIdx + dim, 3 * T(tetra_idx, 2) + dim) = phi(2);
+			N.coeffRef(3 * vertexIdx + dim, 3 * T(tetra_idx, 3) + dim) = phi(3);
+		}
+    }
 }
--- src/phi_linear_tetrahedron.cpp
+++ src/phi_linear_tetrahedron.cpp
@@ -2,5 +2,19 @@
 
 void phi_linear_tetrahedron(Eigen::Vector4d &phi, Eigen::Ref<const Eigen::MatrixXd> V, Eigen::Ref<const Eigen::RowVectorXi> element, Eigen::Ref<const Eigen::Vector3d> x) {
 
-    
+    Eigen::Vector3d deltaX = x - V.row(element(0)).transpose();
+    Eigen::Vector3d delta1 = V.row(element(1)).transpose() - V.row(element(0)).transpose();
+    Eigen::Vector3d delta2 = V.row(element(2)).transpose() - V.row(element(0)).transpose();
+    Eigen::Vector3d delta3 = V.row(element(3)).transpose() - V.row(element(0)).transpose();
+
+    Eigen::Matrix3d T;
+    T.col(0) = delta1;
+    T.col(1) = delta2;
+    T.col(2) = delta3;
+
+    Eigen::Vector3d temp_phi = T.inverse() * deltaX;
+    phi(0) = 1 - temp_phi.sum();
+    phi(1) = temp_phi(0);
+    phi(2) = temp_phi(1);
+    phi(3) = temp_phi(2);
 }
