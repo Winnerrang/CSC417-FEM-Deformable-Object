63fed4e70dc4c4e05a6d5c671fe110d0f97a66ba
Leo Hanxu <leo.hanxu@mail.utoronto.ca>
2024-01-12 18:23:40 -0500

    fix size not matched bug

--- src/T_linear_tetrahedron.cpp
+++ src/T_linear_tetrahedron.cpp
@@ -3,8 +3,18 @@
 #include <iostream>
 void T_linear_tetrahedron(double &T, Eigen::Ref<const Eigen::VectorXd> qdot, Eigen::Ref<const Eigen::RowVectorXi> element, double density, double volume) {
 
-    
+
     Eigen::Matrix1212d M;
+
 	mass_matrix_linear_tetrahedron(M, qdot, element, density, volume);
-	T = 0.5 * qdot.transpose() * M * qdot;
+
+    
+    Eigen::Vector12d qdotj;
+
+    for (int vertexIdx = 0; vertexIdx < 4; vertexIdx++){
+
+        qdotj.segment<3>(3 * vertexIdx) = qdot.segment<3>(3 * element(vertexIdx));
+    }
+	T = 0.5 * qdotj.transpose() * M * qdotj;
+
 }
